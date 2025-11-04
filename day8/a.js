39ec21280b739e82ac2544fe9fc03a91302b410e
Wai Yan Naung <60054637+wynaung19@users.noreply.github.com>
2025-11-04 14:08:15 +0900

    #

---
+++ day8/a.js
@@ -0,0 +1,102 @@
+// console.log("Wai Yan" + " Naung");
+// console.log(10 + 5);
+// console.log(2 - 1);
+// console.log(4 * 2);
+// console.log(10 / 2);
+// console.log(10 % 3);
+// console.log('This is "String"');
+// console.log("Wai Yan\nNaung");
+// console.log("Wai Yan\tNaung");
+// console.log("Wai Yan\\\\Naung");
+// console.log("Hello"[0]);
+// console.log("Hello"[1]);
+// console.log("Hello"[2]);
+// console.log(`Wai Yan Naung`);
+
+// console.log(`Sum: ${10 + 5}`);
+// console.log(`Year : ${new Date().getFullYear()}`);
+
+// console.log(4 > 5);
+// console.log(4 < 5);
+// console.log(4 >= 5);
+// console.log(4 <= 5);
+// console.log(4 == 5);
+// console.log(4 != 5);
+// console.log(4 === 5);
+// console.log(4 !== 5);
+
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
+
+// console.log("a" < "b");
+// console.log(3 < 2 == 1);
+// console.log(3 > 2 && 2 > 1);
+
+// let hours = new Date().getHours();
+// console.log(typeof hours);
+
+// console.log(hours < 3 || 8 < hours);
+// console.log(3 <= hours && hours <= 8);
+
+// let pi = 3.141592653589793;
+// let radius = 5;
+// let area = pi * radius * radius;
+// let circumference = 2 * pi * radius;
+
+// console.log(`Circumference of Circle: ${circumference}`);
+// console.log(`Area of Circle: ${area}`);
+
+// let output = 0;
+// output += 5;
+// output *= 2;
+// console.log(output);
+
+// let o = "hello";
+// o += " world";
+// o += "!";
+// console.log(o);
+
+// let n = 10;
+// console.log(n);
+// console.log(n++);
+// console.log(++n);
+// console.log(n--);
+// console.log(--n);
+// console.log(n);
+
+// console.log(Number("52"));
+// console.log(Number(false));
+// console.log(Number(true));
+// console.log(String(52));
+// console.log(Number("WaiYanNaung"));
+
+// let nan = Number("WaiYanNaung");
+// console.log(isNaN(nan));
+// console.log(nan == nan);
+// console.log(nan === nan);
+// console.log(nan != nan);
+
+// console.log(Boolean(1));
+// console.log(Boolean(-1));
+// console.log(Boolean(0));
+// console.log(Boolean("hello"));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(undefined));
+
+// console.log("52" - 200);
+// console.log("52" * 200);
+// console.log("52" / 200);
+// console.log("52" % 200);
+
+console.log(`52 == "52" : ${52 == "52"}`);
+console.log(`52 === "52" : ${52 === "52"}`);
+console.log();
+console.log(`0 == "" : ${0 == ""}`);
+console.log(`0 === "" : ${0 === ""}`);
+console.log(`0 == false : ${0 == false}`);
+console.log(`0 === false : ${0 === false}`);
+console.log(`null == undefined : ${null == undefined}`);
+console.log(`null === undefined : ${null === undefined}`);
